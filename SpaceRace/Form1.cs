f9a523bbfcd84ac97ee2664be6e77517980f13f3
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-12-15 11:15:57 -0500

    Add project files.

---
+++ SpaceRace/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Space Racing Game
+//December 2022
+//Jasmine Josan
+namespace SpaceRace
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(130, 280, 20, 20);
+        Rectangle player2 = new Rectangle(400, 280, 20, 20);
+
+        SolidBrush goldBrush = new SolidBrush(Color.Goldenrod);
+
+        int player1Score = 0;
+        int player2Score = 0;
+        int player1Speed = 4;
+        int player2Speed = 4;
+        int speed;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+            }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(goldBrush, player1);
+            e.Graphics.FillRectangle(goldBrush, player2);
+
+
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move player 1
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += player1Speed;
+            }
+
+            //move player 2
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += player2Speed;
+            }
+            
+            //Player makes it to the other side
+            if (player1.X < 0)
+            {
+                player1Score++;
+               // p1ScoreLabel.Text = $"{player2Score}";
+
+                player1.X = 130;
+                player1.Y = 280;
+            }
+
+            Refresh();
+        }
+    }
+}
